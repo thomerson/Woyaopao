d06cc28a41e818aa30ce2a41412606db4f3dbfd9
thomerson <tanghongsheng123@126.com>
2018-12-02 10:42:46 +0800

    wechat js sdk

---
+++ Thomerson.Wechat/Thomerson.Wechat.JSSDK/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using Senparc.CO2NET;
+using Senparc.CO2NET.RegisterServices;
+using Senparc.Weixin;
+using Senparc.Weixin.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Thomerson.Wechat.JSSDK
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+
+            var isGLobalDebug = false;//设置全局 Debug 状态
+            var senparcSetting = SenparcSetting.BuildFromWebConfig(isGLobalDebug);
+            var register = RegisterService.Start(senparcSetting).UseSenparcGlobal();//CO2NET全局注册，必须！
+
+            var isWeixinDebug = false;//设置微信 Debug 状态
+            var senparcWeixinSetting = SenparcWeixinSetting.BuildFromWebConfig(isWeixinDebug);
+            register.UseSenparcWeixin(senparcWeixinSetting, senparcSetting);////微信全局注册，必须！
+
+       
+        }
+    }
+}
